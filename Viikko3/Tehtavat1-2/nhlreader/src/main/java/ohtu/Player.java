a34445b0a77270737cc4e5585fa65b2fae575672
Susanna Lauttia <susanna.lauttia@gmail.com>
2020-04-20 21:33:42 +0300

    Kansallisuus lisatty luokkamuuttujiin

--- Viikko3/Tehtavat1-2/nhlreader/src/main/java/ohtu/Player.java
+++ Viikko3/Tehtavat1-2/nhlreader/src/main/java/ohtu/Player.java
@@ -6,6 +6,15 @@ public class Player {
     private String team;
     private int goals;
     private int assists;
+    private String nationality;
+    
+     public Player(String name, String team, int goals, int assists, String nationality) {
+        this.name = name;
+        this.team = team;
+        this.goals = goals;
+        this.assists = assists;
+        this.nationality = nationality;
+    }
 
     public void setName(String name) {
         this.name = name;
@@ -17,7 +26,7 @@ public class Player {
 
     @Override
     public String toString() {
-        return name+" team "+team+" goals "+goals+" assists "+assists;
+        return name+"("+nationality+")"+" team "+team+" goals "+goals+" assists "+assists;
     }
       
 }
